7d665a98992d77a4d997b8500d92cbdcd5a4af91
jonytalukdar <jonytalukdar30@gmail.com>
2021-12-30 11:44:07 +0600

    added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdJAG7d8a_4hX4m95z7xFKp4_O5qskcHk',
+  authDomain: 'what-sapp-clone12.firebaseapp.com',
+  projectId: 'what-sapp-clone12',
+  storageBucket: 'what-sapp-clone12.appspot.com',
+  messagingSenderId: '787731890831',
+  appId: '1:787731890831:web:8d7a5d3deb7c507ce526fa',
+};
+
+const app = initializeApp(firebaseConfig);
